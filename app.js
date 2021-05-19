98507803a198a2e1f7a588c7c9a821f45caebdd2
GUANYI608 <74897537+GUANYI608@users.noreply.github.com>
2021-05-19 21:16:02 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -90,6 +90,14 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .then(() => res.redirect(`/restaurants/${id}`))
     .catch(error => console.log(error))
 })
+// Delete: 刪除特定餐廳
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log('error'))
+})
 // 顯示搜尋結果
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
